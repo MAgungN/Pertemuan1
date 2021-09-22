e73ed986d2f28b1e673f7c413f8ede6bf0404fc2
MAgungN <91149873+MAgungN@users.noreply.github.com>
2021-09-22 10:45:21 +0700

    Add files via upload

---
+++ minggu1.php
@@ -0,0 +1,27 @@
+<?php 
+$nama ="Muhamad Agung Nurrasyid";
+$kelas ="MI20 B";
+$alamat ="Sukaratu, Tasikmalaya";
+$no ="081572100045";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pertemuan 1 - M Agung N</title>
+</head>
+<body>
+	<?php 
+	echo "MY BIODATA";
+	echo "<br>";
+    echo "<br>";
+	echo "Nama      : ".$nama;
+	echo "<br>";
+	echo "Kelas     : ".$kelas;
+    echo "<br>";
+	echo "Alamat    : ".$alamat;
+    echo "<br>";
+	echo "No. HP    : ".$no;
+	?>
+</body>
+</html>
